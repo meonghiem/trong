a6291e4c8904935fc79fe50f343ff5465da637e5
meonghiem <meonghiem@gmail.com>
2023-04-26 23:53:36 +0700

    refactor code and home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from './pages/home';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './components/Home';
-import { useSelector } from 'react-redux';
-import { selectIsLogin } from './pages/loginSlice';
-import { Navigate } from 'react-router-dom';
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/home/Home";
+import Layout from "./components/common/layout";
+import { useSelector } from "react-redux";
+import { selectIsLogin } from "./pages/login/loginSlice";
+import { Navigate } from "react-router-dom";
 
 function App() {
   const isLogin = useSelector(selectIsLogin);
@@ -20,17 +21,23 @@ function App() {
           {isLogin ? (
             <>
               <Route exact path="/" element={<Navigate replace to="/home" />} />
-              <Route exact path="/signup" element={<Navigate replace to="/home" />} />
+              <Route
+                exact
+                path="/signup"
+                element={<Navigate replace to="/home" />}
+              />
               <Route exact path="*" element={<Navigate to="/home" />} />
             </>
           ) : (
             <>
-              <Route exact path='/' element={<Login />} />
-              <Route exact path='/signup' element={<SignUp />} />
+              <Route exact path="/" element={<Login />} />
+              <Route exact path="/signup" element={<SignUp />} />
               <Route path="*" element={<Login />} />
             </>
           )}
-          <Route path='/home' element={<Home />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       <ToastContainer />
---
+++ src/components/card/CardContainer.js
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+
+// type CardContainerProps{
+//   img: String
+//   name: String
+//   maHp: String
+//   endDate: String
+// }
+export default function CardContainer(props) {
+  // const theme = useTheme();
+  // const { imgUrl, name, maHp, endDate } = props;
+  return (
+    <div style={{ border: "1px solid gray", borderRadius: "5px" }}>
+      <Card sx={{ display: "flex" }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 200 }}
+          image={props.imgUrl}
+          alt="Live from space album cover"
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h5">
+              {props.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {props.maHp}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Ended - {props.endDate}
+            </Typography>
+          </CardContent>
+          {/* <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+          <IconButton aria-label="previous">
+            {theme.direction === "rtl" ? (
+              <SkipNextIcon />
+            ) : (
+              <SkipPreviousIcon />
+            )}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {theme.direction === "rtl" ? (
+              <SkipPreviousIcon />
+            ) : (
+              <SkipNextIcon />
+            )}
+          </IconButton>
+        </Box> */}
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Start</Button>
+          </CardActions>
+        </Box>
+      </Card>
+    </div>
+  );
+}
