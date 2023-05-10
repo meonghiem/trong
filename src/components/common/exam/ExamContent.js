82e61dd0b504d7c2383c47a27f12eef99c29524a
trieunguyentm <trieunguyen241102@gmail.com>
2023-05-11 03:16:59 +0700

    Hoan thien chuc nang hien thi bai thi, lam bai, submit

--- src/components/common/exam/ExamContent.js
+++ src/components/common/exam/ExamContent.js
@@ -1,4 +1,4 @@
-import { Box, Paper } from "@mui/material";
+import { Box, Button, Grid, Paper } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Question from "../question/Question";
@@ -7,7 +7,19 @@ export default function ExamContent(props) {
     //props.id
     const id = props.id;
     const [listQuestion, setListQuestion] = useState([]);
+    const [listCorrectAnswer, setListCorrectAnser] = useState(Array(listQuestion.length).fill(false));
     const [exam, setExam] = useState({});
+
+    useEffect(() => {
+        // Khởi tạo giá trị cho listCorrectAnswer
+        if (listQuestion.length > 0) {
+            setListCorrectAnser(Array(listQuestion.length).fill(false));
+        }
+    }, [listQuestion]);
+
+    useEffect(() => {
+        console.log(listCorrectAnswer);
+    }, [listCorrectAnswer])
 
     useEffect(() => {
         async function fetchData() {
@@ -25,6 +37,17 @@ export default function ExamContent(props) {
         fetchData();
     }, [id])
 
+    const handleCallBack = async (index, state) => {
+        const newCheckedList = [...listCorrectAnswer];
+        newCheckedList[index] = state;
+        await setListCorrectAnser(newCheckedList);
+    }
+
+    function handleSubmit() {
+        const count = listCorrectAnswer.filter((answer) => answer === true).length;
+        console.log(`Số câu trả lời đúng là: ${count}`);
+    }
+
     return (
         <div>
             <Paper variant="24" sx={{ marginTop: '20px', marginLeft: '40px', marginRight: '40px' }}>
@@ -33,16 +56,33 @@ export default function ExamContent(props) {
                     <div className="header font-weight-bold font h4" style={{ color: "black", marginLeft: '20px' }}>Title Exam : <span>{exam.title}</span></div>
                 </Box>
                 {/* Hiển thị nội dung câu hỏi */}
-                {listQuestion.map((item, id) => (
-                    <div className="mt-3" key={item.id}>
+                {listQuestion.map((item, index) => (
+                    <div className="mt-3" key={index}>
                         <Question
                             id={item.id}
+                            index={index} // Truyền index cho component con
                             quizType={item.quiz_type}
                             answerList={item.answer_list}
                             keyList={item.key_list}
+                            onChangeAnswer={(index, state) => handleCallBack(index, state)} // Truyền hàm callback cho component con
                         />
                     </div>
                 ))}
+                {/* Nộp bài */}
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button
+                                variant="contained"
+                                sx={{ marginBottom: '10px' }}
+                                className="icon-button"
+                                onClick={handleSubmit}
+                            >
+                                Submit
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Paper>
         </div>
     )
--- src/components/common/question/Question.js
+++ src/components/common/question/Question.js
@@ -1,5 +1,5 @@
-import { Checkbox, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Box, Button, Checkbox, FormControl, FormControlLabel, Grid, Radio, RadioGroup } from "@mui/material";
+import React, { useState } from "react";
 
 export default function Question(props) {
     const id = props.id;
@@ -8,50 +8,64 @@ export default function Question(props) {
     const arrAnswerList = JSON.parse(answerList);
     const keyList = props.keyList;
     const arrKeyList = JSON.parse(keyList);
+    const index = props.index;
+    const [save, setSave] = useState(false);
 
     // Khởi tạo state để lưu trữ trạng thái của các checkbox
     const [checkedList, setCheckedList] = useState(
         Array(arrAnswerList.length).fill(false)
     );
-    // Cách 1:
-    // isCorrect sẽ kiểm tra số lượng giá trị True với số lượng key
-    // Trong biểu thức thứ 2, sẽ trả về false nếu tồn tại 1 đáp án không được chọn, mà đáp án này lại thuộc vào danh sách đáp án chính xác
-    const isCorrect = arrKeyList.length === checkedList.filter(checked => checked).length
-        && checkedList.every((checked, index) => checked || !arrKeyList.includes(arrAnswerList[index]));
-    // Cách 2:
-    // Kiểm tra isCorrect, đầu tiên ta kiểm tra số giá trị True trong checkedList với số lượng đáp án
-    // const isCorrect = arrKeyList.length === checkedList.filter(checked => checked).length
-    //     && checkedList.every((checked, index) => {
-    //         // Nếu đáp án tại vị trí index được chọn kiểm tra xem nó thuộc danh sách đáp án hay không
-    //         if (checked) {
-    //             if (arrKeyList.includes(arrAnswerList[index])) {
-    //                 return true;
-    //             }
-    //             else {
-    //                 return false;
-    //             }
-    //         }
-    //         // Nếu đáp án tại vị trí index không được chọn, kiểm tra xem nếu nó cũng không thuộc danh sách đáp án thì trả về true
-    //         else {
-    //             if (!arrKeyList.includes(arrAnswerList[index])) {
-    //                 return true;
-    //             }
-    //             else {
-    //                 return false;
-    //             }
-    //         }
-    //     });
 
-    useEffect(() => {
-        console.log(isCorrect);
-    }, [isCorrect])
+    // Khởi tạo state để lưu trữ giá trị đáp án được chọn trong RadioGroup
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+
+    // Biến isCorrectCheckBox sẽ kiểm tra số lượng giá trị True với số lượng key
+    // Trong biểu thức thứ 2, sẽ trả về false nếu tồn tại 1 đáp án không được chọn, mà đáp án này lại thuộc vào danh sách đáp án chính xác
+    const isCorrectCheckBox = arrKeyList.length === checkedList.filter(checked => checked).length
+        && checkedList.every((checked, index) => checked || !arrKeyList.includes(arrAnswerList[index]));
+
+    // Biến isCorrectRadio kiểm tra xem đáp án được chọn chính xác hay không
+    const isCorrectRadio = arrKeyList.includes(selectedAnswer);
+
+    // useEffect(() => {
+    //     if (quizType === "multiple_choice") {
+    //         console.log(`id_question: ${id}, result: ${isCorrectCheckBox}`);
+    //         props.onChangeAnswer(index, isCorrectCheckBox);
+    //     }
+    //     else {
+    //         console.log(`id_question: ${id}, result: ${isCorrectRadio}`);
+    //         props.onChangeAnswer(index, isCorrectRadio);
+    //     }
+    // }, [isCorrectCheckBox, isCorrectRadio, quizType, id, index])
 
     // Hàm xử lý khi checkbox thay đổi trạng thái
-    const handleCheckboxChange = async (index) => {
+    const handleCheckChange = async (index) => {
+        if (save) {
+            setSave(!save);
+        }
         const newCheckedList = [...checkedList];
         newCheckedList[index] = !newCheckedList[index];
         await setCheckedList(newCheckedList);
     };
+
+    // Hàm xử lý khi radio thay đổi trạng thái
+    const handleRadioChange = async (event) => {
+        if (save) {
+            setSave(!save);
+        }
+        const value = event.target.value;
+        await setSelectedAnswer(value);
+    };
+
+    const handleSave = () => {
+        setSave(!save);
+        if (quizType === "multiple_choice") {
+            props.onChangeAnswer(index, isCorrectCheckBox);
+        }
+        else {
+            props.onChangeAnswer(index, isCorrectRadio);
+        }
+    }
 
     return (
         <div>
@@ -60,23 +74,64 @@ export default function Question(props) {
                 <Grid item xs={12} sx={{ paddingLeft: '20px' }}>
                     <strong>ID: {id}</strong>
                 </Grid>
+                {/* Hiển thị danh sách đáp án */}
                 <Grid container sx={{ paddingTop: '10px' }}>
-                    {/* Hiển thị danh sách đáp án */}
-                    {quizType === "multiple_choice" && (
-                        <Grid container>
-                            {arrAnswerList.map((answer, index) => (
-                                <Grid item xs={12} key={index} sx={{ paddingTop: '10px' }}>
-                                    <Checkbox
-                                        checked={checkedList[index]}
-                                        onChange={() => handleCheckboxChange(index)}
-                                    />
-                                    {answer}
+                    {
+                        (quizType === "multiple_choice") ?
+                            (
+                                <Grid container>
+                                    {
+                                        arrAnswerList.map((answer, index) => (
+                                            <Grid item xs={12} key={index} sx={{ paddingTop: '10px' }}>
+                                                <Checkbox
+                                                    checked={checkedList[index]}
+                                                    onChange={() => handleCheckChange(index)}
+                                                />
+                                                {answer}
+                                            </Grid>
+                                        ))
+                                    }
                                 </Grid>
-                            ))}
-                        </Grid>
-                    )}
+                            )
+                            :
+                            (
+                                <Grid container sx={{ paddingLeft: '10px' }}>
+                                    <FormControl component="fieldset">
+                                        <RadioGroup
+                                            name={`answer_${id}`}
+                                            value={selectedAnswer}
+                                            onChange={handleRadioChange}
+                                        >
+                                            {arrAnswerList.map((answer, index) => (
+                                                <Grid item xs={12} key={index} sx={{ paddingTop: '10px' }}>
+                                                    <FormControlLabel
+                                                        value={answer}
+                                                        control={<Radio />}
+                                                        label={answer}
+                                                    />
+                                                </Grid>
+                                            ))}
+                                        </RadioGroup>
+                                    </FormControl>
+                                </Grid>
+                            )
+                    }
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box sx={{ paddingLeft: '10px', paddingTop: '10px' }}>
+                            <Button
+                                variant="contained"
+                                sx={{ marginBottom: '10px' }}
+                                className="icon-button"
+                                onClick={handleSave}
+                            >
+                                {save ? "Saved" : "Save"}
+                            </Button>
+                        </Box>
+                    </Grid>
                 </Grid>
             </Grid>
-        </div>
+        </div >
     )
 }
